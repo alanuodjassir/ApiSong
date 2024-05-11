5567e6ecb1179ae6881f14c98324b741dc4a2b31
Alanoud <alanoud@Alanouds-MacBook-Pro.local>
2024-05-12 02:02:49 +0300

    updates the configure

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -4,21 +4,46 @@ import FluentPostgresDriver
 import Vapor
 
 // configures your application
-public func configure(_ app: Application) async throws {
+import Fluent
+import FluentPostgresDriver
+import Vapor
+
+// configures your application
+public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
-        tls: .prefer(try .init(configuration: .clientDefault)))
-    ), as: .psql)
-
+  
+    if let urlString =  Environment.get("DATA_URL"),
+       
+        var postgresConfig = PostgresConfiguration(url: urlString) {
+       
+        var tlsConfig = TLSConfiguration.makeClientConfiguration()
+        tlsConfig.certificateVerification = .none
+        postgresConfig.tlsConfiguration = tlsConfig
+        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
+        
+    } else {
+        
+        
+        app.databases.use(.postgres(
+            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+            port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
+            username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+            password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+            database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+        ), as: .psql)
+        
+    }
+    
+    
     app.migrations.add(CreateSongs())
-    try app.autoMigrate().wait()
+   
+ 
+    if app.environment == .development {
+        
+        try app.autoMigrate().wait()
+    }
     // register routes
     try routes(app)
 }
